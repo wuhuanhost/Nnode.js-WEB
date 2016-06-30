0e0c5e6adba6737a5a8e274b200eca1847838e8f
wuhuanhost <post.thinking@gmail.com>
2016-06-30 12:09:51 +0800

    new version

--- app/app.js
+++ app/app.js
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'webapp')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 获取连接
 var connection = mongoose.getConeection();
